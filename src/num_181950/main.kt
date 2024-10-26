4e0ae40edb62206e1b90e7c68eafb95dfc79a0d6
youtkddnjs <youtkddnjs91@gmail.com>
2024-10-26 12:17:31 +0900

    241026-01

---
+++ src/num_181950/main.kt
@@ -0,0 +1,7 @@
+package num_181950
+
+fun main(){
+    print("input string :")
+    val s1 = readLine()!!
+    print(s1)
+}
---
+++ src/num_181952/main.kt
@@ -0,0 +1,6 @@
+package num_181952
+
+fun main(args: Array<String>) {
+    val (a, b) = readLine()!!.split(' ').map(String::toInt)
+    println("a = $a\nb = $b")
+}
