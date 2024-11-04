5e0ca804099d3c900e826f3fe5617b0976869718
youtkddnjs <youtkddnjs91@gmail.com>
2024-11-04 22:52:56 +0900

    241104-03

---
+++ src/num_181940/main.kt
@@ -0,0 +1,14 @@
+package num_181940
+
+fun main() {
+    val k = 3
+    val my_string = "hello"
+    var answer: String = ""
+
+    for( i in 0..3) {
+        answer += my_string
+    }
+
+    println(answer)
+
+}
