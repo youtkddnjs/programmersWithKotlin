49d531c1f35d2c6aec69e8fd95178650e4ef5e6e
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-08 22:23:52 +0900

    250108-01

--- src/num_181924/main.kt
+++ src/num_181924/main.kt
@@ -2,5 +2,12 @@ package num_181924
 
 fun main() {
 
+    val qeuries : Array<Array<Any>> = arrayOf(
+        arrayOf(0,3),
+        arrayOf(1,2),
+        arrayOf(1,4)
+    )
+
+    [[0, 3],[1, 2],[1, 4]]
     print("hi")
 }
