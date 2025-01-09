1f359c1dbf6f5bcdd3608e4b8d9332220abd245f
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-10 08:39:22 +0900

    250110-01

---
+++ src/num_181915/main.kt
@@ -0,0 +1,4 @@
+package num_181915
+
+class main {
+}
--- src/num_181924/main.kt
+++ src/num_181924/main.kt
@@ -2,12 +2,29 @@ package num_181924
 
 fun main() {
 
+    val arr : Array<Int> = arrayOf(0,1,2,3,4)
     val qeuries : Array<Array<Any>> = arrayOf(
         arrayOf(0,3),
         arrayOf(1,2),
         arrayOf(1,4)
     )
+    var answer: IntArray = intArrayOf()
+    fun mainGo(){
+        var a = 0
+        var b = 0
+        for( i in qeuries){
+            println("s")
+            for( j in i.indices ){
 
-    [[0, 3],[1, 2],[1, 4]]
-    print("hi")
+                if( j == 0 ){
+                    a = arr[i[j] as Int]
+                }else{
+                    b = arr[i[j] as Int]
+                }
+            }
+            println("e")
+        }
+    }
+    mainGo()
 }
+
