012ac059587e2cfc668319d5a64e0831547bc393
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-18 23:17:17 +0900

    250118-05

--- src/num_181920/main.kt
+++ src/num_181920/main.kt
@@ -1,4 +1,21 @@
 package num_181920
 
-class main {
+fun main(){
+    val min = 1
+    val max = 1000
+    val rn = (min..max).random()
+    var answer: IntArray = intArrayOf()
+    var x = 10
+
+    answer = answer.plus(x)
+    while( x > 1 ){
+        if( x % 2 == 0){
+            x = x / 2
+        }else{
+            x = (3 * x) + 1
+        }
+        println(x)
+        answer = answer.plus(x)
+    }
+    print("답 : " + answer.joinToString())
 }
