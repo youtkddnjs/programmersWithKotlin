c31ffe965db99e1186034be92a2b1e2cab246722
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-19 00:11:39 +0900

    250119-01

--- src/num_181918/main.kt
+++ src/num_181918/main.kt
@@ -1,4 +1,21 @@
 package num_181918
 
-class main {
+fun main() {
+    val arr = intArrayOf(1, 4, 2, 5, 3)
+    var stk: IntArray = intArrayOf()
+    var i = 0
+
+    while( i < arr.size){
+        if ( stk.size == 0 ){
+            stk = stk.plus(arr[i])
+            i += 1
+        } else if (stk.last() < arr[i]){
+            stk = stk.plus(arr[i])
+            i += 1
+        } else {
+            stk = stk.dropLast(1).toIntArray()
+        }
+    }
+
+    print(stk.joinToString())
 }
