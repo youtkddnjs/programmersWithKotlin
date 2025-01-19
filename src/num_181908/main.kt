7ab1179e11ea231539fa8a0120ec2a58e4fa8997
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-19 23:12:00 +0900

    250119-13

--- src/num_181908/main.kt
+++ src/num_181908/main.kt
@@ -1,4 +1,18 @@
 package num_181908
 
-class main {
+fun main() {
+    val my_string = "banana"
+    val is_suffix = "ana"
+    var answer: Int = 0
+
+    var s = my_string.length - is_suffix.length
+    var e = my_string.length
+
+    if ( my_string.substring(s,e).contains(is_suffix) ) {
+        answer = 1
+    }else {
+        answer = 0
+    }
+
+    println(answer)
 }
