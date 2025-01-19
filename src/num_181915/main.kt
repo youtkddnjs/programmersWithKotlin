10a0e7f1b239e34df9bb0bdb488634f75837346f
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-19 18:15:37 +0900

    250119-06

--- src/num_181915/main.kt
+++ src/num_181915/main.kt
@@ -1,4 +1,13 @@
 package num_181915
 
-class main {
+fun main(){
+
+    val my_string = "cvsgiorszzzmrpaqpe"
+    val index_list = listOf(16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7)
+    var new_string = ""
+
+    for ( i in index_list)
+        new_string += my_string[i]
+
+    println(new_string)
 }
