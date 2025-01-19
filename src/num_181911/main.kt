765a76d645ee5c7d1a822cfb5943b51dc484680e
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-19 22:06:05 +0900

    250119-10

--- src/num_181911/main.kt
+++ src/num_181911/main.kt
@@ -1,4 +1,22 @@
 package num_181911
 
-class main {
+fun main(){
+    val my_strings = arrayOf("progressive", "hamburger", "hammer", "ahocorasick")
+    val parts = arrayOf(
+        arrayOf(0,4),
+        arrayOf(1,2),
+        arrayOf(3,5),
+        arrayOf(7,7),
+    )
+    var answer = ""
+
+    for ( i in 0..(parts.size-1) ){
+        val s = parts[i][0]
+        val e = parts[i][1]
+        val sliced = my_strings[i].slice(s..e)
+
+        answer += sliced
+    }
+
+    println(answer)
 }
