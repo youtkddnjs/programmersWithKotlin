c1da3245371c130194097fc3358174df64479f67
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-19 21:57:11 +0900

    250119-09

--- src/num_181912/main.kt
+++ src/num_181912/main.kt
@@ -1,4 +1,20 @@
 package num_181912
 
-class main {
+fun main() {
+    val intStrs = arrayOf("0123456789","9876543210","9999999999999")
+    val k = 50000
+    val s = 5
+    val l = 5
+    var result: IntArray = intArrayOf()
+
+    for( i in intStrs){
+        val sliced = i.slice(s..s+l-1)
+        val slicedtoInt = sliced.toInt()
+        if (slicedtoInt > k ){
+            result = result.plus(slicedtoInt)
+        }
+    }
+
+    println("result : ${result.contentToString()}")
+
 }
