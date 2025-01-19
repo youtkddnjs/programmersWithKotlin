79e536a6c6b34c8a920570d30bc20f9a4c79f211
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-19 22:22:05 +0900

    250119-12

--- src/num_181909/main.kt
+++ src/num_181909/main.kt
@@ -1,4 +1,15 @@
 package num_181909
 
-class main {
+fun main() {
+
+    val my_string = "banana"
+    var answer: Array<String> = arrayOf<String>()
+
+    for( i in 0 .. my_string.length - 1){
+        answer = answer.plus(my_string.slice(i..my_string.length-1))
+    }
+    answer = answer.sorted().toTypedArray()
+
+    println(answer.joinToString())
+
 }
