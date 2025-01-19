11b4c4c37bebf9266d7ccc75c1f45e2207002123
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-19 22:13:27 +0900

    250119-11

--- src/num_181910/main.kt
+++ src/num_181910/main.kt
@@ -1,4 +1,12 @@
 package num_181910
 
-class main {
+fun main(){
+    val my_string = "ProgrammerS123"
+    val n = 11
+
+    var answer = ""
+
+    answer = my_string.substring( my_string.length - n , my_string.length )
+
+    println(answer)
 }
