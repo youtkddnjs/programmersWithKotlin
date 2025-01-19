0be69cd79a60fd06eade3fb9b0ac53bae93f811e
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-19 18:27:03 +0900

    250119-07

--- src/num_181914/main.kt
+++ src/num_181914/main.kt
@@ -1,4 +1,13 @@
 package num_181914
 
-class main {
+fun main() {
+
+    val num = "78720646226947352489"
+    var sum = 0
+
+    for (i in num){
+        sum += i.digitToInt()
+    }
+
+    println(sum%9)
 }
