6362b061559d3bb1d20aa8a824ebfbc402c453a0
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-27 11:01:56 +0900

    250127-01

--- src/num_181905/main.kt
+++ src/num_181905/main.kt
@@ -1,5 +1,14 @@
 package num_181905
 
 fun main() {
+    val my_string = "Progra21Sremm3"
+    val s = 6
+    val e = 12
 
+    var sub_string = my_string.substring(s, e+1)
+    println(sub_string)
+    var reversed_string = sub_string.reversed()
+    println(reversed_string)
+    var result = my_string.replaceRange(s,e+1,reversed_string)
+    println(result)
 }
