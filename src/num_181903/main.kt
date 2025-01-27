48c0fbc3d82f67d06a5786b0e60172fbaaa776b3
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-27 16:51:54 +0900

    250127-03

--- src/num_181903/main.kt
+++ src/num_181903/main.kt
@@ -1,4 +1,15 @@
 package num_181903
 
-class main {
+fun main() {
+    val q = 3
+    val r = 1
+    val code = "qjnwezgrpirldywt"
+    var answer = ""
+    for ( i in code.indices){
+        if( i % q == r){
+            answer += code[i]
+
+        }
+    }
+    println(answer)
 }
