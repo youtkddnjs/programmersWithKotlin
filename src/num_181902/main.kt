fa46110c2f82cc16c680ac195ebc40daaa07aa2d
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-27 17:19:48 +0900

    250127-04

--- src/num_181902/main.kt
+++ src/num_181902/main.kt
@@ -1,4 +1,26 @@
 package num_181902
 
-class main {
+fun main() {
+    val my_string = "Programmers"
+    val array = Array(52) { 0 }
+    for (c in my_string) {
+        val stringIndex = c.code
+
+        if( 65<= stringIndex && stringIndex <= 90 ){
+            //대문자
+            for ( i in 65 .. 90){
+                if(stringIndex == i){
+                    array[stringIndex - 65] +=  1
+                }
+            }
+        }else if ( 96<= stringIndex && stringIndex <= 122){
+            //소문자
+            for ( i in 96 .. 122){
+                if(stringIndex == i){
+                    array[stringIndex - 96 + 25] +=  1
+                }
+            }
+        }
+    }
+    println(array.joinToString(separator = ""))
 }
