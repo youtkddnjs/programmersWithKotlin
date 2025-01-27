37ab750d1b9e1184747d8311a1b3cf1cae092211
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-27 18:11:48 +0900

    250127-05

--- src/num_181901/main.kt
+++ src/num_181901/main.kt
@@ -1,4 +1,14 @@
 package num_181901
 
-class main {
+fun main() {
+    val n = 10
+    val k = 3
+    var answer: IntArray = intArrayOf()
+
+    for ( i in 1 until n ) {
+        if( i % k ==0 ){
+            answer = answer.plus(i)
+        }
+    }
+    println(answer.joinToString())
 }
