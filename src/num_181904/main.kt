2fc48cc940ab022679faab55745eea94ef7545f7
youtkddnjs <youtkddnjs91@gmail.com>
2025-01-27 11:28:57 +0900

    250127-02

--- src/num_181904/main.kt
+++ src/num_181904/main.kt
@@ -1,4 +1,19 @@
 package num_181904
 
-class main {
+fun main() {
+    val my_string = "ihrhbakrfpndopljhygc"
+    val m = 4
+    val c = 2
+    var printString = ""
+    var answer = ""
+    for ( i in 0 .. my_string.length -1 ){
+        printString = printString + my_string[i]
+        if ( (i+1) % m == 0) {
+            println( "$printString")
+            answer += printString[c-1]
+            printString = ""
+        }
+    }
+    println("")
+    println(answer)
 }
