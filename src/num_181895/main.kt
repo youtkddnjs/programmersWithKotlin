bd3f2e95104547880fdd6a8905898681475b26cc
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-03 21:55:28 +0900

    250203-06

--- src/num_181895/main.kt
+++ src/num_181895/main.kt
@@ -1,4 +1,17 @@
 package num_181895
 
-class main {
+fun main() {
+    val arr = arrayOf( 1,2,3,4,5)
+    val intervals = arrayOf(
+        arrayOf(1,3),
+        arrayOf(0,4)
+    )
+    var answer : IntArray = intArrayOf()
+
+    for ( i in intervals){
+        answer = answer.plus(arr.slice( i[0] .. i[1] ))
+    }
+
+    println(answer.joinToString())
+
 }
