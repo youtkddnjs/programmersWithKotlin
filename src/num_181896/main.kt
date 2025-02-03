438c63eb09331a3f39afc097c8aed69c53d1741e
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-03 21:45:06 +0900

    250203-05

--- src/num_181896/main.kt
+++ src/num_181896/main.kt
@@ -1,4 +1,11 @@
 package num_181896
 
-class main {
+fun main(){
+    val num_list = arrayOf( 12,4,15,46,38,-2,15)
+    var answer = -1
+    for ( i in num_list.indices){
+        if ( num_list[i] < 0){
+            answer = i
+        }
+    }
 }
