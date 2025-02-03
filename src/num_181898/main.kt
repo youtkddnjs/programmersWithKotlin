7b948cb1267630d8870f4cdc05356c84e743b038
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-03 21:17:32 +0900

    250203-03

--- src/num_181898/main.kt
+++ src/num_181898/main.kt
@@ -1,4 +1,18 @@
 package num_181898
 
-class main {
+fun main() {
+    val arr = arrayOf(1,1,1,1,0)
+    var idx = 3
+    var answer = -1
+    for ( i in arr.indices ){
+        if( idx > arr.size -1 ) break
+        if ( arr[idx] == 1 ){
+            answer = idx
+            break
+        }else{
+            idx++
+        }
+    }
+
+    println(answer)
 }
