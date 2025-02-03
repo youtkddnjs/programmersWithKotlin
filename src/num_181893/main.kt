50bb519e0f76c2984ae09b55cfcdfadc6f65cb04
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-03 22:36:16 +0900

    250203-08

--- src/num_181893/main.kt
+++ src/num_181893/main.kt
@@ -1,4 +1,22 @@
 package num_181893
 
-class main {
+fun main() {
+    val arr = intArrayOf( 0, 1, 2, 3, 4, 5 )
+    val query = intArrayOf( 4, 1, 2)
+
+    var answer = arr
+    for ( i in query.indices) {
+        println( "$i 번째 ${answer.joinToString()} ")
+        if ( i % 2 == 0 ){
+            //짝수 인덱스
+            answer = answer.sliceArray( 0 .. (query[i]) )
+            println( "$i 번째 짝수 ${query[i]} :: ${answer.joinToString()} ")
+        }else {
+            //홀수 인덱스
+            answer = answer.sliceArray((query[i]) .. answer.lastIndex )
+            println( "$i 번째 홀수 ${query[i]} :: ${answer.joinToString()} ")
+        }
+    }
+
+    println( "결과 : ${answer.joinToString()} ")
 }
