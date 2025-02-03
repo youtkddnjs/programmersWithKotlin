c7e61a19fd451e7b1796e0c2c84939dae1df38a3
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-03 22:17:30 +0900

    250203-07

--- src/num_181894/main.kt
+++ src/num_181894/main.kt
@@ -1,4 +1,32 @@
 package num_181894
 
-class main {
+fun main() {
+    val arr = arrayOf(1,1,1)
+    val lastIndex = arr.lastIndex
+    var s_num = -1
+    var e_num = 0
+    var answer : IntArray = intArrayOf()
+
+    for (i in 0 .. lastIndex){
+        if ( arr[i] == 2) {
+            s_num = i
+            break
+        }
+    }
+    for (i in lastIndex downTo 0) {
+        if ( arr[i] == 2) {
+            e_num = i
+            break
+        }
+    }
+    if(s_num != -1 && e_num != 0 ){
+        answer = answer.plus(arr.slice(s_num..e_num))
+    }else{
+        answer = answer.plus(s_num )
+    }
+
+    println(answer.joinToString())
+
+
+
 }
