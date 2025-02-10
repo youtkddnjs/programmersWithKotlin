f5b1104bf260ff9a2fa8309b398319f5307d6537
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-10 22:33:22 +0900

    250210-02

--- src/num_181890/main.kt
+++ src/num_181890/main.kt
@@ -1,4 +1,35 @@
 package num_181890
 
 fun main() {
+
+    val str_list = arrayOf("u", "u", "l", "r")
+    var answer: Array<String> = arrayOf<String>()
+    var index = 0
+    var type = true
+
+    for (i in str_list.indices) {
+
+        if (str_list[i] == "l"){
+            index = i
+            type = true
+            break
+        }else if(str_list[i] == "r"){
+            index = i
+            type = false
+            break
+        }
+    }
+
+    if(type){
+        answer = answer.plus(str_list.slice(0 until  index))
+    }else{
+        answer = answer.plus(str_list.slice(index + 1 ..  str_list.lastIndex))
+    }
+
+
+    println(answer.joinToString())
+
+
+
+
 }
