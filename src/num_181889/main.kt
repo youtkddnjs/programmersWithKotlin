21202c57dbbdf3b43f3fe4230985fc0e769f105e
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-10 22:44:39 +0900

    250210-04

--- src/num_181889/main.kt
+++ src/num_181889/main.kt
@@ -1,4 +1,12 @@
 package num_181889
 
 fun main() {
+    val num_list = arrayOf(5, 2, 1, 7, 5)
+    val n = 3
+    var answer: IntArray = intArrayOf()
+
+
+    answer = answer.plus(num_list.slice(0 until n ))
+
+    println(answer.joinToString())
 }
