8a5f7281560e6f8db5c12b2107f7e0ae333c23f3
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-10 23:11:50 +0900

    250210-05

--- src/num_181888/main.kt
+++ src/num_181888/main.kt
@@ -1,4 +1,18 @@
 package num_181888
 
 fun main() {
+    val num_list = arrayOf(4, 2, 6, 1, 7, 6)
+    val n = 2
+    var answer: IntArray = intArrayOf()
+    var num = n
+
+    answer = answer.plus(num_list[0])
+    for ( i in num_list.indices) {
+        if( i  == num){
+            answer = answer.plus(num_list[i])
+            num += num
+        }
+    }
+
+    println(answer.joinToString())
 }
