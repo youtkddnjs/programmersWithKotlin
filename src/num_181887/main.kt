3e1b2e9e342782b1dcd5f5761fed79d8062e5b03
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-18 20:43:14 +0900

    250218-01

--- src/num_181887/main.kt
+++ src/num_181887/main.kt
@@ -1,4 +1,18 @@
 package num_181887
 
 fun main() {
+    val num_list = arrayOf(4, 2, 6, 1, 7, 6)
+    var answer : Int
+    var l_num = 0
+    var r_num = 0
+
+    for (i in num_list.indices) {
+        if(i%2==0){
+            l_num += num_list[i]
+        }else{
+            r_num += num_list[i]
+        }
+    }
+    answer = if ( l_num > r_num ) l_num else r_num
+    println( "left $l_num right $r_num" )
 }
