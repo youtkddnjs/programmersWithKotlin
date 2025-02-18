a737ae82127fa281fc5f28eccea4614cce9a687e
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-18 20:52:45 +0900

    250218-03

--- src/num_181886/main.kt
+++ src/num_181886/main.kt
@@ -1,4 +1,15 @@
 package num_181886
 
 fun main() {
+    val names = arrayOf("nami", "ahri", "jayce", "garen", "ivern", "vex", "jinx")
+
+    var answer: Array<String> = arrayOf<String>()
+
+    for (i in names.indices) {
+      if( i % 5 == 0){
+          answer += names[i]
+      }
+    }
+
+    println(answer.joinToString())
 }
