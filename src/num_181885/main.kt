643b8d8f557fbb8ad463221c5199b8dc6fd9ee14
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-18 21:47:11 +0900

    250218-04

--- src/num_181885/main.kt
+++ src/num_181885/main.kt
@@ -1,4 +1,14 @@
 package num_181885
 
-class main {
+fun main() {
+    val todo_list = arrayOf("problemsolving", "practiceguitar", "swim", "studygraph")
+    val finished = arrayOf(true, false, true, false)
+    var answer: Array<String> = arrayOf<String>()
+
+    for (i in todo_list.indices) {
+
+        if (!finished[i]) answer += todo_list[i]
+    }
+
+
 }
