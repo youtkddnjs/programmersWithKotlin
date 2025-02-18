d8e38a95f44f87c5d3a6ad55ef446921296a97ab
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-18 21:53:32 +0900

    250218-05

--- src/num_181884/main.kt
+++ src/num_181884/main.kt
@@ -1,4 +1,14 @@
 package num_181884
 
 fun main() {
+    val numbers = arrayOf(34, 5, 71, 29, 100, 34)
+    val n = 123
+
+    var sum = 0
+    for (i in numbers) {
+        sum += i
+        if ( sum > n ) break
+    }
+
+    println(sum)
 }
