ad3bd5d3521f8140b2536280907b720d3d3bb0f3
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-21 22:42:39 +0900

    250221-05

--- src/num_181879/main.kt
+++ src/num_181879/main.kt
@@ -1,4 +1,17 @@
 package num_181879
 
 fun main() {
+    var num_list = intArrayOf(3, 4, 5, 2, 5, 4, 6, 7, 3, 7, 2, 2, 1)
+    var answer: Int = 0
+
+    if (num_list.size > 10){
+        for (i in num_list){
+            answer += i
+        }
+    }else{
+        answer += 1
+        for (i in num_list){
+            answer *= i
+        }
+    }
 }
