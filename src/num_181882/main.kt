9291f6a83addcbf087afe1ee3a4913417fae4e3b
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-21 20:42:29 +0900

    250221-02

--- src/num_181882/main.kt
+++ src/num_181882/main.kt
@@ -1,4 +1,30 @@
 package num_181882
 
 fun main() {
+    val arr = intArrayOf(1, 2, 3, 100, 99, 98)
+    var answer = mutableListOf<Int>()
+
+    // 1,000,000
+    // 30
+    //
+
+    //arr.size 를 10으로 나눈 나머지가 0 일때 아닐때
+    // 0일때면 10 으로 나눈 수 의 10, 20, 30 까지
+    // 나눈수 의
+
+    for ( i in arr.indices ) {
+        answer.add(calNum(arr[i]))
+    }
+
+    println(answer)
+}
+
+fun calNum(num:Int): Int {
+    return if( num % 2 == 0 && (num > 50 || num == 50) ){
+            num / 2
+        }else if ( num % 2 == 1 && num < 50){
+            num * 2
+        }else{
+            num
+        }
 }
