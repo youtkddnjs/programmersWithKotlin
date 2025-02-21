60d42f6a9e42d4acb51cc7b9a45b4a0d50d86712
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-21 22:37:59 +0900

    250221-04

--- src/num_181880/main.kt
+++ src/num_181880/main.kt
@@ -1,4 +1,27 @@
 package num_181880
 
 fun main() {
+
+    val num_list = intArrayOf(12, 4, 15, 1, 14)
+    var answer: Int = 0
+    for (i in num_list) {
+        answer += calNum(i)
+    }
+
+    println(answer)
+}
+
+fun calNum(num:Int): Int {
+    var s = num
+    var count = 0
+    while (s > 1 ){
+        if( s % 2 == 0 ){
+            count++
+            s = s / 2
+        }else{
+            count++
+            s = (s - 1) / 2
+        }
+    }
+    return count
 }
