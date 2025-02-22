4ffda85429c2cde28228b5ce5f73f829734b4205
youtkddnjs <youtkddnjs91@gmail.com>
2025-02-22 20:49:28 +0900

    250221-06

--- src/num_181878/main.kt
+++ src/num_181878/main.kt
@@ -1,4 +1,19 @@
 package num_181878
 
 fun main() {
+    var myString = "aaAA"
+    var myString2 = "AbCdEfG"
+    var pat = "aaaaa"
+    var pat2 = "aBc"
+
+    val answer = myString.contains(pat , ignoreCase = true)
+    val answer2 = myString2.contains(pat2 , ignoreCase = true)
+
+    println(answer)
+    println(answer2)
+
+    val str = "Hello World"
+
+    println("World" in str)  // ✅ 출력: true
+    println("world" in str.lowercase())  // 🚫 출력: false (대소문자 구분)
 }
