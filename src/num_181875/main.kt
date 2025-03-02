f4f4eeb94dd95c35059070e4f9fb60e7b5229bc6
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-02 15:37:22 +0900

    250302-03

--- src/num_181875/main.kt
+++ src/num_181875/main.kt
@@ -1,4 +1,16 @@
 package num_181875
 
 fun main() {
+    val strArr = arrayOf("AAA","BBB","CCC","DDD")
+    var answer: Array<String> = arrayOf<String>()
+
+    for (i in strArr.indices) {
+        if (i % 2 != 0){
+            answer = answer.plus(strArr[i].uppercase())
+        }else{
+            answer = answer.plus(strArr[i].lowercase())
+        }
+    }
+
+    println(answer.joinToString())
 }
