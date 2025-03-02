cfdf17dfe15a1a736976a8e43b0bdfdb34747974
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-02 17:51:30 +0900

    250302-09

--- src/num_181869/main.kt
+++ src/num_181869/main.kt
@@ -1,4 +1,22 @@
 package num_181869
 
 fun main() {
+    var my_string = "i love you"
+    var answer: Array<String> = arrayOf<String>()
+    var temp = ""
+
+    for (i in my_string) {
+        if (i.toString() == " ") {
+            answer = answer.plus(temp)
+            temp = ""
+        } else {
+            temp += i.toString()
+        }
+    }
+
+    answer = answer.plus(temp)
+
+
+    println(answer.joinToString())
+
 }
