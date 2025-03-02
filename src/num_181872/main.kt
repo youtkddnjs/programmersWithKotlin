e74e4bc05f0563f951b575716d1f1305b66240d6
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-02 16:38:56 +0900

    250302-06

--- src/num_181872/main.kt
+++ src/num_181872/main.kt
@@ -1,4 +1,11 @@
 package num_181872
 
 fun main() {
+    val myString = "AbCdEFG"
+    val pat = "dE"
+
+    var answer = myString.substring(0,myString.indexOf(pat)+pat.length)
+
+    println(myString.indexOf(pat))
+
 }
