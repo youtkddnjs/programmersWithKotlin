d37681d590f582989fe806d9181109de68c29e33
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-02 17:40:31 +0900

    250302-08

--- src/num_181870/main.kt
+++ src/num_181870/main.kt
@@ -1,4 +1,18 @@
 package num_181870
 
 fun main() {
+    val strArr = arrayOf("and","notad","abcd")
+    val ad = "ad"
+
+    var answer: Array<String> = arrayOf<String>()
+
+    for (i in strArr) {
+        if (i.contains(ad)) {
+
+        }else{
+            answer = answer.plus(i)
+        }
+    }
+
+    println(answer.joinToString())
 }
