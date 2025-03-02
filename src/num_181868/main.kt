530c3d9d0dc9de4361d9613b5c7a29574c275275
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-02 18:01:11 +0900

    250302-10

--- src/num_181868/main.kt
+++ src/num_181868/main.kt
@@ -1,4 +1,25 @@
 package num_181868
 
 fun main() {
+    var my_string = " programmers "
+    var answer: Array<String> = arrayOf<String>()
+    var temp = ""
+
+    for (i in my_string) {
+        if (i.toString() == " ") {
+            if(temp == ""){
+                temp = ""
+            }else{
+                answer = answer.plus(temp)
+                temp = ""
+            }
+        } else {
+            temp += i.toString()
+        }
+    }
+
+    if(temp != ""){ answer = answer.plus(temp)}
+
+
+    println(answer.joinToString())
 }
