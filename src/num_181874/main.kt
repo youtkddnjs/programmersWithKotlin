cd3bccb33b9deb1598bec5310a006a83329ab4ef
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-02 15:42:59 +0900

    250302-04

--- src/num_181874/main.kt
+++ src/num_181874/main.kt
@@ -1,5 +1,18 @@
 package num_181874
 
 fun main() {
+    val myString = "PrOgRaMmErS"
+
+
+    var answer = ""
+    var lowerString = myString.lowercase()
+    for ( c in lowerString) {
+        if ( c == 'a'){
+            answer += c.uppercase()
+        }else {
+            answer += c
+        }
+    }
+    println(answer)
 
 }
