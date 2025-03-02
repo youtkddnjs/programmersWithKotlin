70807614e069da72ce7a77ef22e101b35d65e366
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-02 15:51:19 +0900

    250302-05

--- src/num_181873/main.kt
+++ src/num_181873/main.kt
@@ -1,4 +1,17 @@
 package num_181873
 
 fun main() {
+    val myString = "programmers"
+    val alp = "p"
+
+    var answer = ""
+
+    for ( c in myString) {
+        if ( c.toString() == alp){
+            answer += c.uppercase()
+        }else {
+            answer += c
+        }
+    }
+    println(answer)
 }
