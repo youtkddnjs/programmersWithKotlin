81685f9ef97d2c41feab743dffefa1f35e6487da
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-04 22:22:36 +0900

    250304-04

--- src/num_181865/main.kt
+++ src/num_181865/main.kt
@@ -1,5 +1,27 @@
 package num_181865
 
 fun main() {
+    val binomial = "4 - 12"
+    var answer = 0
+    if(binomial.contains("+")){
+        val bi = binomial.split("+").map{
+            it.trim()
+        }
+        answer = bi[0].toInt() + bi[1].toInt()
+    }else if(binomial.contains("-")){
+        val bi = binomial.split("-").map{
+            it.trim()
+        }
+        answer = bi[0].toInt() - bi[1].toInt()
 
+    }else if(binomial.contains("*")){
+        val bi = binomial.split("*").map{
+            it.trim()
+        }
+        answer = bi[0].toInt() * bi[1].toInt()
+
+    }
+
+
+    println(answer)
 }
