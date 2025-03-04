daad1b31794ee584b09619f08a63526eeadea40e
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-04 22:01:44 +0900

    250304-03

--- src/num_181866/main.kt
+++ src/num_181866/main.kt
@@ -1,4 +1,24 @@
 package num_181866
 
 fun main() {
+    val myString = "dxccxbbbxxaaaaxaaa"
+    var result = mutableListOf<String>()
+    var tempString = ""
+
+    for (c in myString) {
+
+        if (c == 'x') {
+            if(!tempString.isNullOrBlank()){
+                result.add(tempString)
+            }
+            tempString = ""       // 그리고 count를 초기화합니다.
+        }else{
+            tempString += c // 각 문자마다 count를 증가시킵니다.
+        }
+    }
+    if(!tempString.isNullOrBlank()){
+            result.add(tempString)
+        }
+    result.sort()
+    println(result)
 }
