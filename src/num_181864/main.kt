63b7db50397cf8d8de16411635e0cdc452d14efe
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-11 23:25:16 +0900

    250311-02

--- src/num_181864/main.kt
+++ src/num_181864/main.kt
@@ -1,4 +1,19 @@
 package num_181864
 
 fun main() {
+    val myString = "ABBAA"
+    val pat = "AABB"
+
+
+    var answer = 0
+    var tap = ""
+    for ( i in pat){
+        if ( i == 'A'){
+            tap += 'B'
+        }else{
+            tap += 'A'
+        }
+    }
+    answer = if(myString.contains(tap)) { 1 } else { 0 }
+    println(answer)
 }
