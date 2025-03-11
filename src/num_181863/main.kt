7ae9ecc011b15a73fec3521ba599a401a5f0d3e5
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-11 23:25:19 +0900

    250311-03

--- src/num_181863/main.kt
+++ src/num_181863/main.kt
@@ -1,4 +1,15 @@
 package num_181863
 
 fun main() {
+    val rny_string = "masterpiece"
+    var answer: String = ""
+
+    for (i in rny_string){
+        if ( i == 'm'){
+            answer += "rn"
+        }else {
+            answer += i
+        }
+    }
+
 }
