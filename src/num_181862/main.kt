334464df9f979c8e14f180385416b556747a24a5
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-11 23:25:29 +0900

    250311-04

--- src/num_181862/main.kt
+++ src/num_181862/main.kt
@@ -1,4 +1,23 @@
 package num_181862
 
 fun main() {
+    val myStr = "abc"
+    var result = mutableListOf<String>()
+    var str = ""
+    for (i in myStr){
+
+        if ( i == 'a' || i == 'b' || i == 'c'){
+            if( str != ""){
+                result.add(str)
+                str = ""
+            }
+        }else{
+            str  += i
+        }
+    }
+    if( str != ""){
+        result.add(str)
+    }
+    if(result.isEmpty()) result.add("EMPTY")
+    println(result.joinToString())
 }
