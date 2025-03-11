358a881967a153a512a20b44cd863f630fa908dd
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-11 23:48:09 +0900

    250311-05

--- src/num_181861/main.kt
+++ src/num_181861/main.kt
@@ -1,4 +1,13 @@
 package num_181861
 
 fun main() {
+    val arr = arrayOf(5, 1, 4)
+    val answer = mutableListOf<Int>()
+    for (i in arr) {
+        for ( j in 1..i) {
+            answer.add(i)
+        }
+    }
+
+    println(answer.joinToString())
 }
