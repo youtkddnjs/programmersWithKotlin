0d8ee80cc31f9c0f69abb1fae0fbc41f33ee84ea
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-13 22:57:36 +0900

    250313-01

--- src/num_181860/main.kt
+++ src/num_181860/main.kt
@@ -1,4 +1,22 @@
 package num_181860
 
 fun main() {
+    var aar = arrayOf(3, 2, 4, 1, 3)
+    var flag = arrayOf(true, false, true, false, false)
+    val answer = mutableListOf<Int>()
+
+    for ( i in flag.indices){
+        if(flag[i]){
+            for( j in 1..aar[i]*2){
+                answer.add(aar[i])
+            }
+        }else{
+            for( j in 1..aar[i]){
+                answer.removeLast()
+            }
+
+        }
+    }
+
+    println(answer.joinToString())
 }
