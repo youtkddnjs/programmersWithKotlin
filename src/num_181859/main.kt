2387c5c88fdd4ca8ad22ebccbfb454a382918457
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-13 23:56:52 +0900

    250313-02

--- src/num_181859/main.kt
+++ src/num_181859/main.kt
@@ -1,4 +1,23 @@
 package num_181859
 
 fun main() {
+    val arr = arrayOf(0, 1, 1, 1, 0)
+    var result = mutableListOf<Int>()
+
+    for (i in arr) {
+        if(result.isEmpty()){
+            result.add(i)
+        }else{
+            if( result.last() == i){
+                result.removeLast()
+            }else{
+                result.add(i)
+            }
+        }
+    }
+    if(result.isEmpty()) {
+        result.add(-1)
+    }
+
+    println(result.joinToString())
 }
