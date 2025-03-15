9cfbe5b694b147373e97df057602122dd6e0d70b
youtkddnjs <youtkddnjs91@gmail.com>
2025-03-15 19:06:24 +0900

    250315-01

--- src/num_181858/main.kt
+++ src/num_181858/main.kt
@@ -1,4 +1,18 @@
 package num_181858
 
 fun main() {
+    val arr = arrayOf(0, 1, 1, 1, 1)
+    val k = 4
+    var answer = mutableListOf<Int>()
+    answer.addAll(arr.distinct())
+
+    if ( answer.size < k){
+        for ( i in 0 .. k - answer.size - 1){
+            answer.add(-1)
+        }
+    }else {
+        answer = answer.slice(0..k-1).toMutableList()
+    }
+
+    println(answer.joinToString())
 }
