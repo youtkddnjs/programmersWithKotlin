1b43ab41d0a261c1db677a2bdc368956fa79f1c7
youtkddnjs <youtkddnjs91@gmail.com>
2025-05-16 22:32:52 +0900

    250516-01

--- src/num_181851/main.kt
+++ src/num_181851/main.kt
@@ -3,6 +3,18 @@ package num_181851
 fun main() {
     val rank = arrayOf(3, 7, 2, 5, 4, 6, 1)
     val attendance = arrayOf(false, true, true, true, true, false, false)
+    var isattendace = mutableListOf<Int>()
+    var count = 0
+    for ( i in 1..rank.size) {
+        var num = rank.indexOf(i)
 
+        if(attendance[num]){
+            count++
+            isattendace.add(num)
+        }
+
+        if (count == 3 ) break
+    }
+    isattendace[0] * 10000 + isattendace[1] * 100 + isattendace[2]
 
 }
